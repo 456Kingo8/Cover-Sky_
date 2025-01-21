da2639a77960104ba4d4fc92401131efc2f9f057
StaratBoard <2847606770@qq.com>
2025-01-21 17:21:13 +0800

    体质基本效果实现

--- content/BodyTrait.cs
+++ content/BodyTrait.cs
@@ -14,6 +14,7 @@ namespace ZheTian.content
     public partial class Trait
     {
         private static ActorTrait _shen_wang_ti { get; set; }
+        private static ActorTrait _fan_gu { get; set; }
         
         private static ActorTrait _xian_tian_dao_tai { get; set; }
 
@@ -38,36 +39,51 @@ namespace ZheTian.content
         [Hotfixable]
         public void InitBodyTrait()
         {
-            TraitAdd(_shen_wang_ti,"ShengWangTi", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_fan_gu,"FanGu", 0.80f, new Dictionary<string, float>()
+            {
+                { S.mod_health,0.05f },
+                { S.mod_health,0.05f },
+                { S.mod_armor,0.01f },
+            },"BodyTraitGroup");
+            
+            TraitAdd(_shen_wang_ti,"ShengWangTi", 0.03f, new Dictionary<string, float>()
             {
                 { S.mod_damage,0.3f },
                 { S.mod_armor,0.03f },
-                { Stats.mod_talent.id,100000f}
             },"BodyTraitGroup");
 
-            TraitAdd(_yuan_ling_ti,"YuanLingTi", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_yuan_ling_ti,"YuanLingTi", 0.03f, new Dictionary<string, float>()
             {
-                { S.health,100 }
+                { Stats.mod_talent.id,10f }
             },"BodyTraitGroup");
             
-            TraitAdd(_xian_tian_dao_tai,"XianTianDaoTai", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_xian_tian_dao_tai,"XianTianDaoTai", 0.005f, new Dictionary<string, float>()
             {
-                { S.health,100 }
+                { S.mod_health, 0.8f },
+                { S.mod_damage, 0.8f },
+                { S.mod_armor, 0.07f },
+                { Stats.mod_talent.id, 0.3f }
             },"BodyTraitGroup");
             
-            TraitAdd(_huang_gu_sheng_ti,"HuangGuShengTi", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_huang_gu_sheng_ti,"HuangGuShengTi", 0.005f, new Dictionary<string, float>()
             {
-                { S.health,100 }
+                { S.mod_health, 0.8f },
+                { S.mod_damage, 0.8f },
+                { S.mod_armor, 0.07f }
             },"BodyTraitGroup");
             
-            TraitAdd(_cang_tian_ba_ti,"CangTianBaTi", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_cang_tian_ba_ti,"CangTianBaTi", 0.005f, new Dictionary<string, float>()
             {
-                { S.health,100 }
+                { S.mod_health, 0.8f },
+                { S.mod_damage, 0.8f },
+                { S.mod_armor, 0.07f }
             },"BodyTraitGroup");
             
-            TraitAdd(_Hun_dun_ti,"HunDunTi", 0.01f, new Dictionary<string, float>()
+            TraitAdd(_Hun_dun_ti,"HunDunTi", 0.0001f, new Dictionary<string, float>()
             {
-                { S.health,100 }
+                { S.mod_health, 1.2f },
+                { S.mod_damage, 1.2f },
+                { S.mod_armor, 0.1f }
             },"BodyTraitGroup");
             
             TraitAdd(_xian_tian_sheng_ti_dao_tai,"XianTianShengTiDaoTai", 0.01f, new Dictionary<string, float>()
@@ -77,24 +93,30 @@ namespace ZheTian.content
 
             TraitAdd(_xu_kong_zhi_ti,"XuKongZhiTi", 0.01f, new Dictionary<string, float>()
             {
-                { S.health, 50 },
-                { S.speed, 100 },
-                { S.intelligence, 50 }
+                { S.mod_health, 0.5f },
+                { S.mod_damage, 0.5f },
+                { S.mod_armor, 0.05f }
             },"BodyTraitGroup");
 
             TraitAdd(_tian_yao_ti,"TianYaoTi", 0.01f, new Dictionary<string, float>()
             {
-
+                { S.mod_health, 0.5f },
+                { S.mod_damage, 0.5f },
+                { S.mod_armor, 0.05f }
             },"BodyTraitGroup");
 
             TraitAdd(_tai_yang_shen_ti,"TaiYangShenTi", 0.01f, new Dictionary<string, float>()
             {
-
+                { S.mod_health, 0.5f },
+                { S.mod_damage, 0.5f },
+                { S.mod_armor, 0.05f }
             },"BodyTraitGroup");
 
             TraitAdd(_tai_yin_shen_ti,"TaiYinShenTi", 0.01f, new Dictionary<string, float>()
             {
-
+                { S.mod_health, 0.5f },
+                { S.mod_damage, 0.5f },
+                { S.mod_armor, 0.05f }
             },"BodyTraitGroup");
 
             ActorTraitGroupAsset group = new ActorTraitGroupAsset()
--- content/EffectTrait.cs
+++ content/EffectTrait.cs
@@ -18,32 +18,122 @@ public partial class Trait  : ExtendLibrary<ActorTrait, Trait>
     
     private void AddEffects()
     {
+        //C级体质实现了基础:血攻加持30%，防御3%（小成，大成）
         _shen_wang_ti.GetExtend().conditional_basestats = a =>
         {
-            return IsDivineKingBodyUpgraded(a);
+            return IsDivineKingBodyUpgraded(a,0.5f,0.7f,0.03f,0.03f);
         };
-
-
+        _yuan_ling_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a, 0.5f, 0.7f, 0.03f, 0.03f);
+        };
+        
+        //B级体质实现了基础:血攻加持50%，防御5%（小成，大成）
+        _xu_kong_zhi_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,0.7f,0.9f,0.05f,0.05f);
+        };
+        _tian_yao_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,0.7f,0.9f,0.05f,0.05f);
+        };
+        _tai_yang_shen_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,0.7f,0.9f,0.05f,0.05f);
+        };
+        _tai_yin_shen_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,0.7f,0.9f,0.05f,0.05f);
+        };
+        
+        //A级体质实现了基础:血攻80%，防御7%（小成，大成）
+        _xian_tian_dao_tai.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,1f,1.5f,0.07f,0.07f);
+        };
+        _cang_tian_ba_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,1f,1.5f,0.07f,0.07f);
+        };
+        _huang_gu_sheng_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,1f,1.5f,0.07f,0.07f);
+        };
+        
+        //S级体质实现了基础:血攻120%，防御10%（小成，大成）
+        _Hun_dun_ti.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,1.5f,2f,0.1f,0.1f);
+        };
+        _xian_tian_sheng_ti_dao_tai.GetExtend().conditional_basestats = a =>
+        {
+            return IsDivineKingBodyUpgraded(a,1.5f,2f,0.1f,0.1f);
+        };
+        
+        
+        //神王体，对比自己低的境界有5%概率秒杀
+        _shen_wang_ti.action_attack_target = (actor, target, tile) =>
+        {
+            if (!target.isActor()) return true;
+            if (actor.a.GetCultisysLevel() > target.a.GetCultisysLevel())
+            {
+                if (Toolbox.randomChance(0.05f))
+                {
+                    target.a.attackedBy = actor;
+                    target.a.killHimself(true, pLaunchCallbacks: false);
+                }
+                return true;
+            }
+            return false;
+        };
+        
+        //天妖体，战斗中变大，攻速加10%，伤害减免10%
+        _tian_yao_ti.action_attack_target = (actor, target, tile) =>
+        {
+            return false;
+        };
+        
+        //太阳神体，攻击附有灼烧
+        _tai_yang_shen_ti.action_attack_target = (actor, target, tile) =>
+        {
+            return false;
+        };
+        
+        //太阴神体，攻击附有冰冻
+        _tai_yin_shen_ti.action_attack_target = (actor, target, tile) =>
+        {
+            return false;
+        };
+        
+        //荒古圣体，攻击附有攻速延缓
+        _huang_gu_sheng_ti.action_attack_target = (actor, target, tile) =>
+        {
+            return false;
+        };
+        //苍天霸体，攻击可能（10%）引发地震
+        _cang_tian_ba_ti.action_attack_target = (actor, target, tile) =>
+        {
+            return false;
+        };
     }
     [Hotfixable]
-    private BaseStats IsDivineKingBodyUpgraded(Actor actor)
+    private BaseStats IsDivineKingBodyUpgraded(Actor actor,float damage,float damage2,float armor = 0f,float armor2 = 0f)
     {
-        Debug.Log("调用函数");
         //小成
         if (actor.GetCultisysLevel() > 4 && actor.GetCultisysLevel() < 11)
         {
-            Debug.Log("升级成功！1111");
             return new BaseStats()
             {
-                [S.mod_damage] = 0.5f,
+                [S.mod_damage] = damage,
+                [S.mod_armor] = armor,
             };
         }
         else if (actor.GetCultisysLevel() >= 11)
         {
-            Debug.Log("升级成功！2222");
             return new BaseStats()
             {
-                [S.mod_damage] = 0.7f,
+                [S.mod_damage] = damage2,
+                [S.mod_armor] = armor2,
             };
         }
         actor.updateStats(); // 更新角色状态
